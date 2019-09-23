78eca4d30b5d7c28076ae20808cf54850f302908
jasvirkaur <jasvir.kaur1224@gmail.com>
2019-09-23 11:41:41 -0400

    First Commit

--- src/main/java/com/example/practise_code/MainActivity.java
+++ src/main/java/com/example/practise_code/MainActivity.java
@@ -30,5 +30,48 @@ public class MainActivity extends AppCompatActivity {
 
         textView1.setText(y);
 
+        // Last 15 Charcaters
+
+        String z = X.substring(X.length()- 15);
+        textView2.setText(z);
+
+        // Total P's
+
+        int count1 = 0 ;
+        char totalp = 'p';
+        // int total1 = 0;
+        for (int i = 0; i < X.length(); i++)
+            if( X.charAt(i) == totalp) {
+                count1++;
+            }
+
+        System.out.println(count1);
+
+
+        String u = String.valueOf(count1);
+        textView3.setText(u);
+
+
+
+
+
+        // Total C's
+
+        int count = 0 ;
+        char totalc = 'c';
+        //int total = 0;
+        for (int i = 0; i < X.length(); i++)
+            if( X.charAt(i) == totalc) {
+                count++;
+            }
+
+        System.out.println(count);
+
+
+        String v = String.valueOf(count);
+        textView4.setText(v);
+
+
+
     }
 }
